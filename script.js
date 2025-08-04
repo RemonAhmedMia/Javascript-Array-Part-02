57f8c2d2ee02075c0fa30cdb8dbd69de6f76598e
TheRemonDev <riyanahmed3536@gmail.com>
2025-08-04 10:40:38 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const friends = ["Elon" , "Faruk" , "Bill" , "Mark" , "Sania"];
+for(const friend of friends) {
+    //console.log(friend); sob gula value dhekabe
+}
+for(let i = 0; i < 10; i++) {
+    console.log(i);
+    console.log(friends[i]);
+}
+
+for (let i = 0; i < friends.length; i ++) {
+    console.log(i);
+    console.log(friends[i]);
+}
