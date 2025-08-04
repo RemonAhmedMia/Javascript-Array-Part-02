efbca353f5efbd99de58739b122a18b896ff066f
TheRemonDev <riyanahmed3536@gmail.com>
2025-08-04 16:07:41 +0100

    short-one

---
+++ reverse.js
@@ -0,0 +1,9 @@
+// reverse() হলো JavaScript-এর array method, যেটা কোন array-র উপাদানগুলোর ক্রম উল্টে দেয় (last থেকে first পর্যন্ত)।
+// এটা মূল array-টাই পরিবর্তন করে (in-place change)।
+
+let number = [1, 2, 3, 4, 5];
+console.log(number);
+
+number.reverse();
+
+console.log(number);  // Output: [5, 4, 3, 2, 1]
---
+++ short.js
@@ -0,0 +1,5 @@
+// sort() হলো JavaScript এর একটা Array method, যেটা array এর উপাদানগুলো alphabetical বা ascending order এ সাজায়।
+
+const team = ["Rimon" , "Al-Amin" , "Badol" , "Shakib" , "Tamanna"];
+const teamMember = team.sort();
+console.log(teamMember);
