764e2803b2b5e56b349b69e176d38293f9da5d38
TheRemonDev <riyanahmed3536@gmail.com>
2025-08-04 11:01:18 +0100

    for and while loop

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const friends = ["Elon" , "Faruk" , "Bill" , "Mark" , "Sania"];
+const friends = ["Elon" , "Faruk" , "Bill" , "Mark" , "Sania" , "Anika"];
 for(const friend of friends) {
     //console.log(friend); sob gula value dhekabe
 }
@@ -10,4 +10,27 @@ for(let i = 0; i < 10; i++) {
 for (let i = 0; i < friends.length; i ++) {
     console.log(i);
     console.log(friends[i]);
+}
+
+
+// Number diye find out
+
+const numbers = [ 1, 2, 34, 84, 90, 36, 110];
+for(let i = 0; i<numbers.length; i++) {
+    console.log(numbers[i]);
+}
+
+// While Loop diye===
+
+let i = 0;
+while (i < friends.length) {
+    console.log(friends[i]);
+    i++
+}
+
+let n = 0;
+while (i < numbers.length) {
+    console.log(numbers[n]);
+    
+    i++
 }
