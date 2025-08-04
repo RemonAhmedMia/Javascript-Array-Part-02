6892128d863873c54fbc5f71deb119951b583987
TheRemonDev <riyanahmed3536@gmail.com>
2025-08-04 16:25:07 +0100

    accesnding

--- short.js
+++ short.js
@@ -3,3 +3,21 @@
 const team = ["Rimon" , "Al-Amin" , "Badol" , "Shakib" , "Tamanna"];
 const teamMember = team.sort();
 console.log(teamMember);
+
+
+// Ascending (ছোট → বড়):
+// const num = [8,4,7,12,90,5,9];
+// const num_acd =num.sort();
+// console.log(num_acd);
+
+
+const num = [8, 4, 7, 12, 90, 5, 9];
+
+const num_acs = num.sort(function (a , b) {
+  return a - b;
+});
+
+console.log(num_acs);
+
+
+
